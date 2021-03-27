8893c6271e620f550ddecefabd287036c727d8be
mikecrbenton <mikecrbenton@hotmail.com>
2021-03-27 11:32:14 -0500

    added password hashing to users seed

--- api/data/seeds/02-users.js
+++ api/data/seeds/02-users.js
@@ -1,18 +1,21 @@
+const bcrypt = require("bcryptjs")
+
+
 exports.seed = async function(knex) {
    await knex('users').insert([
      {
          username: 'Joe Smith',
-         password: 'Lambda1',
+         password:  await bcrypt.hash("Lambda1", 12),
          phone_number: '111-111-1111' 
      },
      {
          username: 'Mary Smith',
-         password: 'Lambda2',
+         password:  await bcrypt.hash("Lambda1", 12),
          phone_number: '222-222-2222' 
      },
      {
          username: 'Kim Smith',
-         password: 'Lambda3',
+         password:  await bcrypt.hash("Lambda1", 12),
          phone_number: '333-333-3333' 
      }
    ]);
